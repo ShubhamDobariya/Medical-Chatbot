71eff41c1ada42915ab1fb2f7ba928d27c9bf51e
Shubham Dobariya <shubhamdobariyas880@gmail.com>
2025-10-19 19:43:20 +0530

    Folder Strcture Created

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Create Directory 
+mkdir -p src
+mkdir -p notebook
+
+# Create files
+touch .env
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch notebook/Medical-Chatbot.ipynb
+touch setup.py
+touch app.py
+touch requirements.txt
+
+
+echo "Directory and Files Created Successfully."
